37e34d045d218b5747ca13da12424b2b66d59591
Lauri Hartikka <lhartikk@gmail.com>
2020-06-07 08:58:27 +0300

    test

--- script.js
+++ script.js
@@ -1,131 +1,79 @@
- var bigCircleRadius = 15;
- var circleRadius = 4.5;
- var diameter = 2500;
+const X_MUL = 1000;
+const Y_MUL = 700;
 
- $(document).ready(function() {
-     var tree = createTree();
-     var svg = createSvg();
+d3.json("moves.json")
+  .then(function (data) {
 
-     d3.json("moves.json", function(error, root) {
-         var nodes = tree.nodes(root),
-             links = tree.links(nodes);
+    const tree = d3.tree();
 
-         createLinks(svg, links);
-         var node = createNodes(svg, nodes);
-         createCircles(node);
-         var nodeText = createTexts(node);
+    const myTree = tree(d3.hierarchy(data));
+    window.myTree = myTree;
 
-         createPrefixMove(nodeText);
-         createHrefs(nodeText);
-         createPostfixMove(nodeText);
-         scrollToCenter();
-     });
 
-     d3.select(self.frameElement).style("height", diameter - 150 + "px");
+    myTree.each((node) => {
+      createCircle(node);
+      if(node.children) {
+        for(let childNode of node.children ) {
+          createLine(node.x * X_MUL, node.y * Y_MUL, childNode.x *X_MUL, childNode.y * Y_MUL);
+        }
+      }
+    })
 
-     $('#start').click(function(e) {
-         scrollToCenter();
-         e.preventDefault();
-     });
- })
+  });
 
- function createTree() {
-     return d3.layout.tree()
-         .size([360, diameter / 2 - 120])
-         .separation(function(a, b) {
-             return (a.parent == b.parent ? 1 : 2) / a.depth;
-         });
- }
+function getNodeIndex(node) {
+  return `h${node.height}d${node.depth}`;
+}
 
- function createLinks(svg, links) {
-     var diagonal = d3.svg.diagonal.radial()
-         .projection(function(d) {
-             return [d.y, d.x / 180 * Math.PI];
-         });
-     svg.selectAll(".link")
-         .data(links)
-         .enter().append("path")
-         .attr("class", "link")
-         .attr("d", diagonal);
- }
+function createCircle(node) {
+  var x = node.x * X_MUL;
+  var y= node.y * Y_MUL;
+  var svg = document.getElementById('svg');
+  var xmlns = "http://www.w3.org/2000/svg";
+  var circle = document.createElementNS(xmlns, 'circle');
 
- function createTexts(nodes) {
-     return nodes.append("text")
-         .attr("dy", ".31em")
-         .attr("text-anchor", function(d) {
-             return d.x < 180 ? "start" : "end";
-         })
-         .attr("transform", function(d) {
-             return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)";
-         })
- }
+  setAttributes(circle, {
+      'cx': x, 'cy':y, 'r' : 2,
+      'stroke' :  'black',
+      'stroke-width' : '4',
+      'id' : getNodeIndex(node)
+  });
 
- function createCircles(nodes) {
-     nodes.append("circle")
-         .attr("r", function(d, i) {
-             if (i == 0) {
-                 return bigCircleRadius;
-             }
-             return circleRadius;
-         })
- }
 
- function createNodes(svg, nodes) {
-     return svg.selectAll(".node")
-         .data(nodes)
-         .enter().append("g")
-         .attr("class", "node")
-         .attr("transform", function(d) {
-             return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")";
-         })
- }
 
- function createSvg() {
-     return d3.select("body").append("svg")
-         .attr("width", diameter)
-         .attr("height", diameter - 150)
-         .append("g")
-         .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
- }
+  var animate = document.createElementNS(xmlns, 'animate');
+  setAttributes(animate, {
+    'xlink:href' : `#${getNodeIndex(node)}`,
+    attributeName: 'cx',
+    attributeType: 'xml',
+    from : 200,
+    to : 1000,
+    dur: '3s',
+    begin: '0s',
+    repeatCount: 1,
+    fill: 'freeze'
+  });
 
- function createHrefs(nodeTexts) {
-     nodeTexts.append("tspan")
-         .attr("font-weight", "200")
-         .append("a")
-         .attr("xlink:href", function(d) {
-             return d.link;
-         })
-         .attr("target", "_blank")
-         .text(function(d) {
-             return (d.name != null ? d.name : "");
-         });
- }
 
- function createPrefixMove(nodeTexts) {
-     nodeTexts.append("tspan")
-         .attr("font-weight", "bold")
-         .text(function(d) {
-             if (d.x < 180) {
-                 return " " + d.move + " ";
-             }
-         });
- }
+  // circle.appendChild(animate);
+  svg.appendChild(circle);
+  animate.beginElement()
 
- function createPostfixMove(nodeTexts) {
-     nodeTexts.append("tspan")
-         .attr("font-weight", "bold")
-         .text(function(d) {
-             if (d.x >= 180) {
-                 return " " + d.move + " ";
-             }
-         })
- }
+}
 
- function scrollToCenter() {
-     var position = $("circle[r='" + bigCircleRadius + "']").offset();
-     $("html, body").animate({
-         scrollTop: (position.top - window.innerHeight / 2) + "px",
-         scrollLeft: (position.left - window.innerWidth / 2) + "px"
-     });
-     return false;
- }
+function createLine(x1, y1, x2, y2) {
+  var svg = document.getElementById('svg');
+  var xmlns = "http://www.w3.org/2000/svg";
+  var line = document.createElementNS(xmlns, 'line');
+  setAttributes(line, {
+    x1, y1, x2,y2,
+    style: 'stroke:black;stroke-width:1'
+  });
+  svg.appendChild(line);
+}
+
+function setAttributes(node, attributes) {
+  for([val, key] of Object.entries(attributes)) {
+    node.setAttribute(val, key);
+  }
+}
